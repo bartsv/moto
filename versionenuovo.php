4a49d0fb420c2a441773baf8f366d221f37f4494
bartsv <b.bartsv@gmail.com>
2015-05-11 22:43:18 +0200

    p
    p

---
+++ versionenuovo.php
@@ -0,0 +1,51 @@
+<?php
+require_once 'utility/cn_db.php';
+getVersion();
+function getVersion(){
+$filename="data.txt";
+$fp = fopen($filename, "r");
+$letto=fread($fp, filesize($filename));
+fclose($fp);
+if(strcmp($letto, "")==0){
+		$fp=fopen($filename, "w");
+		if (flock($fp, LOCK_EX)) { // Esegue un lock esclusivo
+		    fwrite($fp, time().'');
+		    flock($fp, LOCK_UN); // rilascia il lock
+		} else {
+		    echo "Non si riesce ad eseguire il lock del file !";
+		}
+		fclose($fp);
+	$db=dbconn();
+	$qu="SELECT * FROM `versioni`;";
+	$sql = $db->prepare($qu);
+	   		 $sql->execute();
+	while($e=$sql->fetch(PDO::FETCH_ASSOC){
+	     $output=$e;
+	}
+	$db=null;
+	file_put_contents("versione.json", json_encode($output));
+}
+else
+	if ($letto+1800<=time()) {
+		$fp=fopen($filename, "w");
+		if (flock($fp, LOCK_EX)) { // Esegue un lock esclusivo
+		    fwrite($fp, time().'');
+		    flock($fp, LOCK_UN); // rilascia il lock
+		} else {
+		    echo "Non si riesce ad eseguire il lock del file !";
+		}
+		fclose($fp);
+	$db=dbconn();
+	$qu="SELECT * FROM `versioni`;";
+	$sql = $db->prepare($qu);
+	   		 $sql->execute();
+	while($e=$sql->fetch(PDO::FETCH_ASSOC){
+	     $output=$e;
+	}
+	$db=null;
+	file_put_contents("versione.json", json_encode($output));
+	}
+         $json_file = file_get_contents('versione.json');
+         echo $json_file
+}
+?>
